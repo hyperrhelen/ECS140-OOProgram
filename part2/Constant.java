abfee7e70d8e87c5bd7944eb54b33fe26b4f5bf1
Helen Chac <hyperrhelen@gmail.com>
2014-10-31 19:48:47 -0700

    Part 2
    Created a function inside the derived classes of Seq that found the
    minimum value

---
+++ part2/Constant.java
@@ -0,0 +1,22 @@
+public class Constant extends Seq{
+  private int num, value;
+
+  Constant( int num, int value){
+    super();
+    this.num = num;
+    this.value = value;
+  }
+  public String toString(){
+  if (this.num == 0){
+    this.value = 0;
+  }
+    return "[ " + this.num + " : " + this.value + " ]" ;
+  }
+
+  public int min(){
+
+    return value;
+  }
+
+
+}
---
+++ part2/Delta.java
@@ -0,0 +1,29 @@
+public class Delta extends Seq{
+  private int num, initial, delta;
+
+  Delta(int num, int initial, int delta){
+    super();
+    this.num = num;
+    this.initial = initial;
+    this.delta = delta;
+  }
+  public String toString(){
+    if (this.num == 0){
+	this.initial = 0;
+	this.delta = 0;
+    }
+    return "< " + this.num + " : " + this.initial + " &" + this.delta + " >";
+  }
+
+  public int min(){
+//    if(delta > initial)
+//    return 0;
+    if (delta < 0){
+      return initial + delta*(num-1);
+    }
+    else
+      return initial;
+//    else
+//      return delta;
+  }
+}// class Delta
