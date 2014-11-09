68559bf75ece65988a98a94097005bb0d0bd7f98
Helen Chac <hyperrhelen@gmail.com>
2014-11-08 16:05:44 -0800

    Exception to Iterators
    Used try, catch statements that are derived from the original Exception
    to try and catch errors.
    Files, JumbleIt.java, JumbleUser.java, ConstantIt.java, DeltaIt.java
    have been modified to fit UsingIteratorPastEndException.java.
    UsingIteratorPastEndException file has been written to show use of the
    exception catching.

---
+++ part4/Jumble.java
@@ -0,0 +1,37 @@
+public class Jumble extends Seq{
+
+  protected int [] values ;
+
+
+  Jumble(int [] values){
+    super();
+    this.values = new int [values.length];
+    for (int i = 0; i < values.length; i++){
+	this.values[i] = values[i];
+    }
+  }
+
+  public String toString(){
+    System.out.print("{ " + values.length + " : ");
+    for (int i = 0; i < this.values.length; i++){
+      System.out.print(this.values[i] + " " );
+    }
+    return "}";
+  } 
+
+  public int min(){
+    int min = 0;
+    if (values.length == 0)
+	return 0;
+    else if (values.length > 0){
+	for (int i = 0; i < values.length; i++){
+	  if (i == 0)
+	      min = values[i];
+	  else if (values[i] < min)
+	      min = values[i];
+        }
+    }
+    return min;
+  }
+
+}//Jumble
